947bc1966eae46959564bd968f0f39db4573356a
AaronSalinasBU <58755799+AaronSalinasBU@users.noreply.github.com>
2020-04-10 14:03:18 -0500

    Submission

---
+++ Week1/App.js
@@ -0,0 +1,18 @@
+import React from "React"
+
+function App(){
+    return (
+        <div>
+            <input type="checkbox"></input>
+            <span>Item 1</span>
+            <br></br>
+            <input type="checkbox"></input>
+            <span>Item 2</span>
+            <br></br>
+            <input type="checkbox"></input>
+            <span>Item 3</span>
+        </div>
+    )
+}
+
+export default App
