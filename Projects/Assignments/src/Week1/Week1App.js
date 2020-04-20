c1ca073e00e82434ecc541eb32877824ef843993
AaronSalinasBU <58755799+AaronSalinasBU@users.noreply.github.com>
2020-04-20 12:07:33 -0500

    commit

---
+++ Projects/Assignments/src/Week1/Week1App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Week1App() {
+  return (
+    <div>
+        <input type="checkbox"></input>
+        <span>Item 1</span>
+        <br></br>
+        <input type="checkbox"></input>
+        <span>Item 2</span>
+        <br></br>
+        <input type="checkbox"></input>
+        <span>Item 3</span>
+    </div>
+)
+}
+
+export default Week1App;
